ba01ef05d727693e11cf0c8d760349aedb8fb15b
ardydedase <ardy.dedase@gmail.com>
2015-09-10 22:15:01 +0800

    Bump version

--- apiwrapper/__init__.py
+++ apiwrapper/__init__.py
@@ -3,10 +3,10 @@
 # @Author: ardydedase
 # @Date:   2015-08-30 11:19:30
 # @Last Modified by:   ardydedase
-# @Last Modified time: 2015-09-09 21:06:37
+# @Last Modified time: 2015-09-10 21:52:56
 
 __author__ = 'Ardy Dedase'
 __email__ = 'ardy.dedase@gmail.com'
-__version__ = '0.1.3'
+__version__ = '0.1.4'
 
 from .apiwrapper import APIWrapper
