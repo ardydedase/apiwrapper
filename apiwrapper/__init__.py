05bc10895584434f5d79d24d1ca07ba4eb0a64aa
ardydedase <ardy.dedase@gmail.com>
2015-09-11 17:15:54 +0800

    Import exceptions and constants

--- apiwrapper/__init__.py
+++ apiwrapper/__init__.py
@@ -3,10 +3,19 @@
 # @Author: ardydedase
 # @Date:   2015-08-30 11:19:30
 # @Last Modified by:   ardydedase
-# @Last Modified time: 2015-09-10 21:52:56
+# @Last Modified time: 2015-09-11 16:57:48
 
 __author__ = 'Ardy Dedase'
 __email__ = 'ardy.dedase@gmail.com'
 __version__ = '0.1.4'
 
-from .apiwrapper import APIWrapper
+from .apiwrapper import (
+    APIWrapper,
+    ExceededRetries,
+    EmptyResponse,
+    InvalidResponse,
+    MissingParameter,
+    InvalidParameter,
+    STRICT,
+    GRACEFUL,
+    IGNORE)
