98ed67c2171a69481604f3abfe7b1db3b5b98338
ardydedase <ardy.dedase@gmail.com>
2015-09-20 15:56:13 +0800

    Version bump

--- setup.py
+++ setup.py
@@ -26,7 +26,7 @@ test_requirements = [
 
 setup(
     name='apiwrapper',
-    version='0.1.5',
+    version='0.1.6',
     description="Simple API Wrapper",
     long_description=readme + '\n\n' + history,
     author="Ardy Dedase",
