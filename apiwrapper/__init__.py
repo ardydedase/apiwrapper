7f4034e36ec1ba989f3718bee0e20204d9fc7b25
ardydedase <ardy.dedase@gmail.com>
2015-09-24 20:18:04 +0800

    Version bump

--- apiwrapper/__init__.py
+++ apiwrapper/__init__.py
@@ -3,11 +3,11 @@
 # @Author: ardydedase
 # @Date:   2015-08-30 11:19:30
 # @Last Modified by:   ardydedase
-# @Last Modified time: 2015-09-14 17:38:35
+# @Last Modified time: 2015-09-24 18:48:07
 
 __author__ = 'Ardy Dedase'
 __email__ = 'ardy.dedase@gmail.com'
-__version__ = '0.1.6'
+__version__ = '0.1.7'
 
 from .apiwrapper import (
     APIWrapper,
--- setup.py
+++ setup.py
@@ -26,7 +26,7 @@ test_requirements = [
 
 setup(
     name='apiwrapper',
-    version='0.1.6',
+    version='0.1.7',
     description="Simple API Wrapper",
     long_description=readme + '\n\n' + history,
     author="Ardy Dedase",
